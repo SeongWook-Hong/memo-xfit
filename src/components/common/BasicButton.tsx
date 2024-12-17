a851bb77ad75d1a6fcb6abe58f609d358f892aec
hong <hsw5971@naver.com>
2024-12-18 00:43:58 +0900

    refactor: 확장 가능성으로 인한 로직 수정

--- src/components/common/BasicButton.tsx
+++ src/components/common/BasicButton.tsx
@@ -1,25 +1,29 @@
 import { ButtonHTMLAttributes } from "react";
 
-type Color = "orange" | "gray" | "white";
+type Color = "orange" | "white";
 type Size = "sm" | "md" | "lg";
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   color: Color;
   size: Size;
 }
-
-const BasicButton = ({ color, size, children, onClick }: Props) => {
+const BasicButton = ({ disabled, color, size, children, onClick }: Props) => {
   const sizes = {
-    sm: `py-2 text-sm min-w-32 md:min-w-24`,
-    md: `py-3 text-lg min-w-72 w-full max-w-[520px]`,
-    lg: `py-5 text-lg min-w-72 w-full max-w-[544px]`,
+    sm: "py-2 text-sm min-w-32 md:min-w-24",
+    md: "py-3 text-lg min-w-72 w-full max-w-[520px]",
+    lg: "py-5 text-lg min-w-72 w-full max-w-[544px]",
+  };
+  const colors = {
+    orange: `${disabled ? "bg-gray" : `bg-${color}`}`,
+    white: `border-[1px] bg-${color} border-gray ${
+      size === "lg" ? "text-black" : "text-orange"
+    } `,
   };
 
   return (
     <button
-      className={`rounded-md bg-${color} ${sizes[size]} ${
-        color === "white" ? "border-[1px] border-gray text-orange" : ""
-      } ${size === "lg" ? "text-black" : ""}`}
+      className={`rounded-md ${colors[color]} ${sizes[size]}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
