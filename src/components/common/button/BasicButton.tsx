3c5307c8a618c725cc3c3770c9391fabc822096b
hong <hsw5971@naver.com>
2024-12-18 18:19:06 +0900

    chore: 저장 위치 수정

--- src/components/common/button/BasicButton.tsx
+++ src/components/common/button/BasicButton.tsx
@@ -1,12 +1,10 @@
 import { ButtonHTMLAttributes } from "react";
 
-type Color = "orange" | "white";
-type Size = "sm" | "md" | "lg";
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  color: Color;
-  size: Size;
+  color: "orange" | "white";
+  size: "sm" | "md" | "lg";
 }
-const BasicButton = ({ disabled, color, size, children, onClick }: Props) => {
+const BasicButton = ({ color, size, disabled, children, onClick }: Props) => {
   const sizes = {
     sm: "py-2 text-sm min-w-32 md:min-w-24",
     md: "py-3 text-lg min-w-72 w-full max-w-[520px]",
@@ -22,8 +20,8 @@ const BasicButton = ({ disabled, color, size, children, onClick }: Props) => {
   return (
     <button
       className={`rounded-md ${colors[color]} ${sizes[size]}`}
-      onClick={onClick}
       disabled={disabled}
+      onClick={onClick}
     >
       {children}
     </button>
