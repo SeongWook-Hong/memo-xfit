32bf1581f606b7dc041d33a7edd68424bda903a5
hong <hsw5971@naver.com>
2025-01-02 23:03:40 +0900

    feat: signin, signup 페이지

---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/page/auth/LoginForm";
+
+const Signin = () => {
+  return (
+    <div className="mx-auto flex flex-col w-[520px] p-2">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Signin;
---
+++ src/pages/auth/signup/index.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/page/auth/LoginForm";
+
+const Signup = () => {
+  return (
+    <div className="mx-auto flex flex-col w-[520px] p-2">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Signup;
