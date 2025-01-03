5d387043e3a0ef58d9b475105970b9318324d8b2
hong <hsw5971@naver.com>
2025-01-03 16:51:02 +0900

    feat: signin page

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,9 +1,18 @@
-import LoginForm from "@/components/page/auth/LoginForm";
+import Link from "next/link";
+import SignForm from "@/components/page/auth/SignForm";
 
 const Signin = () => {
   return (
-    <div className="mx-auto flex flex-col w-[520px] p-2">
-      <LoginForm />
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="mx-auto flex flex-col w-[520px] p-3 gap-3">
+        <SignForm sign="in" />
+        <span className="text-center">
+          회원이 아니신가요?{" "}
+          <Link href="/auth/signup" className="text-orange underline">
+            회원가입 하기
+          </Link>
+        </span>
+      </div>
     </div>
   );
 };
