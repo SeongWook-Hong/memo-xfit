cb9a0d9374695bb0d0929f44b0fa057d5fa41dd9
hong <hsw5971@naver.com>
2025-02-03 22:25:27 +0900

    chore: naming 수정

--- src/hooks/postSignin.ts
+++ src/hooks/postSignin.ts
@@ -1,7 +1,7 @@
 import baseAxios from "@/lib/axios";
 
-const getSignin = async (formData: object) => {
+const postSignin = async (formData: object) => {
   const { data } = await baseAxios.post("/user/signin", formData);
   return data;
 };
-export default getSignin;
+export default postSignin;
