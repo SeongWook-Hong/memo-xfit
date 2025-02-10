887d5fb8d0b0c773e033aa966d69d1e1fd3dd3c6
hong <hsw5971@naver.com>
2025-02-10 20:55:39 +0900

    feat: 로그인 상태 확인 util 추가 - checkSignin

---
+++ src/utils/checkSignin.ts
@@ -0,0 +1,15 @@
+import { GetServerSidePropsContext } from "next";
+import { getCookie } from "cookies-next";
+import jwt from "jsonwebtoken";
+
+export const checkSignin = async (context: GetServerSidePropsContext) => {
+  const token = await getCookie("loginToken", {
+    req: context.req,
+    res: context.res,
+  });
+  if (token) {
+    const decode = jwt.verify(token, process.env.JWT_SECRET!) as jwt.JwtPayload;
+    return { props: { nickname: decode.nickname } };
+  }
+  return { props: {} };
+};
