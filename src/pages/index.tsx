96fca653ad677c16e6d814faaba20143f53cc5d0
hong <hsw5971@naver.com>
2025-02-10 20:56:07 +0900

    feat: 로그인 상태 확인 SSR 대응

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div className="text-2xl">test</div>;
+import Header from "@/components/common/header/Header";
+import { checkSignin } from "@/utils/checkSignin";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return await checkSignin(context);
+};
+interface Props {
+  nickname: string | undefined;
+}
+export default function Home({ nickname }: Props) {
+  return (
+    <div className="text-2xl">
+      <Header nickname={nickname} />
+    </div>
+  );
 }
