f0bf9787f2479394720384ef00618f07be7564ba
hong <hsw5971@naver.com>
2025-02-12 23:12:40 +0900

    chore: auth 페이지 logo 변경

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,7 +1,9 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
+import Image from "next/image";
 import { useRef } from "react";
 import SignForm from "@/components/page/auth/SignForm";
+import logoPic from "@/assets/images/logo-curved.png";
 import { authRedirect } from "@/lib/authRedirect";
 import { useGetUsedEmail, usePostSignup } from "@/hooks/useAuth";
 
@@ -25,8 +27,8 @@ const Signup = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="mx-auto flex flex-col w-[520px] p-3 gap-3">
-        <Link href="/" className="mx-auto text-6xl pb-7">
-          MEMO-XFIT
+        <Link href="/">
+          <Image src={logoPic} width={496} alt="logo Image curved" />
         </Link>
         <SignForm sign="up" formRef={formRef} onSubmit={handleSubmit} />
         <span className="text-center">
