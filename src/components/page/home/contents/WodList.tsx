8a4bbacc4471dbbceeb20322fdfddfcf65d26905
hong <hsw5971@naver.com>
2025-02-18 22:03:54 +0900

    chore: scroll-bar css 수정

--- src/components/page/home/contents/WodList.tsx
+++ src/components/page/home/contents/WodList.tsx
@@ -9,12 +9,12 @@ const WodList = ({ wods }: Props) => {
       {wods.map((wod: TWod) => (
         <li
           key={wod._id}
-          className="flex flex-col gap-3 p-6 rounded-xl bg-[--componentBgColor] w-56 h-56 flex-shrink-0"
+          className="flex flex-col gap-4 p-6 rounded-xl bg-[--componentBgColor] w-56 h-56 flex-shrink-0"
         >
           <span className="text-[--orange] font-bold">{wod.date}</span>
-          <pre className="flex flex-col gap-1 text-base">
+          <pre className="flex flex-col gap-1 text-base h-32">
             <span className="font-bold">{wod.wodType + ":\n"}</span>
-            <span className="overflow-x-hidden text-ellipsis overflow-y-scroll">
+            <span className="overflow-x-hidden text-ellipsis overflow-y-scroll h-full overflow-hidden-scrollbar">
               {wod.workout}
             </span>
           </pre>
