3b789176987090449c0ce723c8599f475eb935dd
hong <hsw5971@naver.com>
2025-02-19 20:49:20 +0900

    style: scroll-bar styling 수정

--- src/components/page/home/contents/WodList.tsx
+++ src/components/page/home/contents/WodList.tsx
@@ -5,22 +5,25 @@ interface Props {
 }
 const WodList = ({ wods }: Props) => {
   return (
-    <ul className="flex gap-5 p-4 overflow-y-auto">
-      {wods.map((wod: TWod) => (
-        <li
-          key={wod._id}
-          className="flex flex-col gap-4 p-6 rounded-xl bg-[--componentBgColor] w-56 h-56 flex-shrink-0"
-        >
-          <span className="text-[--orange] font-bold">{wod.date}</span>
-          <pre className="flex flex-col gap-1 text-base h-32">
-            <span className="font-bold">{wod.wodType + ":\n"}</span>
-            <span className="overflow-x-hidden text-ellipsis overflow-y-scroll h-full overflow-hidden-scrollbar">
-              {wod.workout}
-            </span>
-          </pre>
-        </li>
-      ))}
-    </ul>
+    <div className="flex flex-col gap-4">
+      <span>Recent WODs</span>
+      <ul className="flex gap-5 overflow-y-auto custom-scrollbar pb-2">
+        {wods.map((wod: TWod) => (
+          <li
+            key={wod._id}
+            className="flex flex-col gap-4 p-6 rounded-xl bg-[--componentBgColor] w-56 h-56 flex-shrink-0"
+          >
+            <span className="text-[--orange] font-bold">{wod.date}</span>
+            <pre className="flex flex-col gap-1 text-base h-32">
+              <span className="font-bold">{wod.wodType + ":\n"}</span>
+              <span className="overflow-x-hidden text-ellipsis overflow-y-scroll h-full overflow-hidden-scrollbar">
+                {wod.workout}
+              </span>
+            </pre>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 export default WodList;
