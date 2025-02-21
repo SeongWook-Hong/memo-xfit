2d38c307fb50cb7035405010bacbf416f80c596c
hong <hsw5971@naver.com>
2025-02-21 19:20:07 +0900

    chore: CSR -> SSR

--- src/pages/wod/[id].tsx
+++ src/pages/wod/[id].tsx
@@ -1,14 +1,17 @@
 import Header from "@/components/page/home/header/Header";
-import { useAuthStore } from "@/store/useAuthStore";
-import { useEffect } from "react";
+import { checkSignin } from "@/lib/checkSignin";
+import { GetServerSideProps } from "next";
 
-const Wod = () => {
-  const { nickName } = useAuthStore();
-
-  useEffect(() => {}, []);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return await checkSignin(context);
+};
+interface Props {
+  nickname: string | undefined;
+}
+const Wod = ({ nickname }: Props) => {
   return (
     <>
-      <Header nickname={nickName}></Header>
+      <Header nickname={nickname}></Header>
       {}
     </>
   );
