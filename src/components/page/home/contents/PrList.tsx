5034c2e66444b93b90c070795c4d43b0582d3640
wook <hsw5971@naver.com>
2025-02-28 21:45:03 +0900

    feat: PrList components UI 제작

--- src/components/page/home/contents/PrList.tsx
+++ src/components/page/home/contents/PrList.tsx
@@ -2,7 +2,29 @@ const PrList = () => {
   return (
     <div className="flex flex-col gap-4">
       <h2>PR List</h2>
-      <div className="p-6 rounded-xl bg-[--componentBgColor] w-full max-w-[464px] h-[336px]"></div>
+      <div className="p-6 rounded-xl bg-[--componentBgColor] w-full max-w-[464px] h-[336px] leading-loose">
+        <p className="">
+          Clean - 225LB <span className="text-sm text-[--orange]">1일 전</span>
+        </p>
+        <p className="">
+          Clean & Jerk - 225LB{" "}
+          <span className="text-sm text-[--orange]">1일 전</span>
+        </p>
+        <p className="">
+          Snatch - 185LB <span className="text-sm text-[--orange]">1일 전</span>
+        </p>
+        <p className="">
+          Deadlift - 405LB{" "}
+          <span className="text-sm text-[--orange]">1일 전</span>
+        </p>
+        <p className="">
+          Squat - 315LB <span className="text-sm text-[--orange]">1일 전</span>
+        </p>
+        <p className="">
+          Shoulder Press - 225LB{" "}
+          <span className="text-sm text-[--orange]">1일 전</span>
+        </p>
+      </div>
     </div>
   );
 };
